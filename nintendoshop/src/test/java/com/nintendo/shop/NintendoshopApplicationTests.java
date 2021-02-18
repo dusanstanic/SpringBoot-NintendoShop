a7fb8351931a7fedbd255f0a58060f277a419492
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-02-18 08:24:39 +0100

    Add resources

---
+++ nintendoshop/src/test/java/com/nintendo/shop/NintendoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nintendo.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NintendoshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
